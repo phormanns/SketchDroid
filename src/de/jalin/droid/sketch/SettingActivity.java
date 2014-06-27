68d1d9ef7858faf80be319179339e5d5244939a5
Peter Hormanns <peter.hormanns@jalin.de>
2014-06-27 11:40:20 +0200

    added preferences for sharing by email

--- src/de/jalin/droid/sketch/SettingActivity.java
+++ src/de/jalin/droid/sketch/SettingActivity.java
@@ -1,13 +1,50 @@
 package de.jalin.droid.sketch;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+
 
 public class SettingActivity extends PreferenceActivity {
 
-	 @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.settings);
-	 }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBuildHeaders(final List<Header> target) {
+        loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+       if (item.getItemId() == android.R.id.home) {
+    	   finish();
+       }
+       return false;
+    }
+    
+    @Override
+    protected boolean isValidFragment(final String fragmentName) {
+    	final String fragmentClassName = EMailPrefsFragment.class.getName();
+		if (fragmentClassName.equals(fragmentName)) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public static class EMailPrefsFragment extends PreferenceFragment {
+    	
+    	@Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.email_preferences);
+        }
+    }
 }
--- src/de/jalin/droid/sketch/SketchActivity.java
+++ src/de/jalin/droid/sketch/SketchActivity.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,7 +24,7 @@ public class SketchActivity extends Activity {
 	private SketchCanvas view;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		if (savedInstanceState != null && savedInstanceState.containsKey("drawing")) {
 			drawing = (Drawing) savedInstanceState.get("drawing");
@@ -36,50 +38,65 @@ public class SketchActivity extends Activity {
 	}
 
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	protected void onSaveInstanceState(final Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putSerializable("drawing", drawing);
 	}
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		getMenuInflater().inflate(R.menu.sketch, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		final int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void reset(MenuItem item) {
+	public void settings(final MenuItem item) {
+		startActivity(new Intent(this, SettingActivity.class));;
+	}
+
+	public void about(final MenuItem item) {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(("http://www.lug-kr.de/wiki/SketchDroid"))));
+	}
+
+	public void reset(final MenuItem item) {
 		view.reset();
 	}
 
-	public void share(MenuItem item) {
-		Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+	public void share(final MenuItem item) {
+		final int width = view.getWidth();
+		final int height = view.getHeight();
+		final int max = width > height ? width : height;
+		final Bitmap bitmap = Bitmap.createBitmap(max, max, Bitmap.Config.ARGB_8888);
 		Canvas canvas = new Canvas(bitmap);
 		canvas.drawColor(0xFFE0E0E0);
 		view.draw(canvas); 
 		try {
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+			final String mailto = preferences.getString("prefMailto", "hello@example.org");
+			final String subject = preferences.getString("prefSubject", "SketchDroid");
+			final String text = preferences.getString("prefText", "");
 			final String picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
 			final File sketchTempDirectory = new File(picturesDirectory + "/sketch_temp");
 			sketchTempDirectory.mkdirs();
-			final File tempFile = new File(sketchTempDirectory.getAbsoluteFile() + "/sketch.jpg");
+			final File tempFile = new File(sketchTempDirectory.getAbsoluteFile() + "/sketch.png");
 			final FileOutputStream outputStream = new FileOutputStream(tempFile);
-			bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream); 
+			bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream); 
 			outputStream.close();
 			final Intent intent = new Intent(Intent.ACTION_SEND);
-			intent.setType("image/jpeg");
+			intent.setType("image/png");
 			intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tempFile));
-			intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "peter.hormanns@jalin.de" } );
-			intent.putExtra(Intent.EXTRA_SUBJECT, "Sketch");
-			intent.putExtra(Intent.EXTRA_TEXT, "Deine Notiz");
-			startActivity(Intent.createChooser(intent, "Sende Notiz"));
+			intent.putExtra(Intent.EXTRA_EMAIL, new String[] { mailto } );
+			intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+			intent.putExtra(Intent.EXTRA_TEXT, text);
+			startActivity(Intent.createChooser(intent, "Send Sketch"));
 		} catch (IOException e) {
 			Log.e("FILE", e.getLocalizedMessage(), e);
 		}
